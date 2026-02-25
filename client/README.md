3daa5667f523a13061c0b603205bde35ff585da1
tkyc <terrykyc.7000@gmail.com>
2026-02-25 01:06:36 -0800

    Initial client code

---
+++ client/README.md
@@ -0,0 +1,1 @@
+./main --target-ip 127.0.0.1 --target-port 8888 --timeout 10 --max-retries 3
---
+++ client/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+static std::string IP;
+static int PORT;
+static int TIMEOUT;
+static int MAX_RETRIES;
+
+enum class Flag {
+    TargetIP,
+    TargetPort,
+    Timeout,
+    MaxRetries,
+    Unknown
+};
+
+struct Argument {
+    Flag flag;
+    void* value;
+    Argument(Flag flag, void* value) : flag(flag), value(value) {}
+};
+
+static const std::unordered_map<std::string, Argument> LEGAL_FLAGS = {
+    {"--target-ip",   {Flag::TargetIP, &IP}},
+    {"--target-port", {Flag::TargetPort, &PORT}},
+    {"--timeout",     {Flag::Timeout, &TIMEOUT}},
+    {"--max-retries", {Flag::MaxRetries, &MAX_RETRIES}}
+};
+
+void parse_args(int argc, char* argv[]) {
+    // Start loop at 1 to skip program name
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+
+        // Check if argv[i + 1] is within bounds
+        if (i + 1 < argc) {
+            switch (LEGAL_FLAGS.count(arg) ? LEGAL_FLAGS.at(arg).flag : Flag::Unknown) {
+                case Flag::TargetIP:
+                    *(static_cast<std::string*>(LEGAL_FLAGS.at(arg).value)) = argv[++i];
+                    break;
+                case Flag::Unknown:
+                    // throw unknown argument error
+                    break;
+                default:
+                    *(static_cast<int*>(LEGAL_FLAGS.at(arg).value)) = std::stoi(argv[++i]);
+                    break;
+            }
+        } else {
+            // throw out of bounds error
+        }
+    }
+}
+
+void print_args() {
+    std::cout << IP << std::endl;
+    std::cout << PORT << std::endl;
+    std::cout << TIMEOUT << std::endl;
+    std::cout << MAX_RETRIES << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    parse_args(argc, argv);
+
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (sockfd < 0) {
+        std::cout << "ERROR CREATING SOCKET..." << std::endl;
+        // throw exception here
+        return 1;
+    }
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, IP.c_str(), &server_address.sin_addr) <= 0) {
+        std::cerr << "INVALID ADDRESS..." << std::endl;
+        return 1;
+    }
+
+    if (connect(sockfd, (struct sockaddr*) &server_address, sizeof(server_address)) < 0) {
+        std::cerr << "CONNECTION FAILED..." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Connected to server!" << std::endl;
+    std::string input;
+
+    while (true) {
+        std::getline(std::cin, input);
+        send(sockfd, input.c_str(), input.length(), 0);
+    }
+
+    close(sockfd);
+
+    return 0;
+}
+
